c01287e6fdf8a641e7e58e83d2ea837e4e61001e
Ranjan Malav <ranjan2192@gmail.com>
2021-07-05 20:52:31 +0530

    Setup app navigation.

--- app/src/main/java/com/ranjan/malav/morselight_flashlightwithmorsecode/MainActivity.kt
+++ app/src/main/java/com/ranjan/malav/morselight_flashlightwithmorsecode/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.ranjan.malav.morselight_flashlightwithmorsecode
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        AppBarConfiguration(
+            setOf(
+                R.id.navigation_send, R.id.navigation_receive, R.id.navigation_learn,
+            )
+        )
+        navView.setupWithNavController(navController)
     }
 }
